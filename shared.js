a6e0eead762cf8fe954460a6819b0d2454449c89
Alexandru-Nicolae DUNĂRENŢU (102451) <alexandru.dunarentu@stud.etti.upb.ro>
2020-08-20 19:56:39 +0200

    hosting website

---
+++ shared.js
@@ -0,0 +1,34 @@
+var backdrop = document.querySelector(".backdrop");
+var selectPlanButtons = document.querySelectorAll(".button");
+var modal = document.querySelector(".modal");
+var disable_modal = document.querySelector(".modal__action--negative");
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    modal.style.display = "block";
+    backdrop.style.display = "block";
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  backdrop.style.display = "none";
+  mobileNav.style.transform = "translateX(-100%)";
+  if(modal){
+  modal.style.display = "none";}
+ // mobileNav.style.display = 'none';
+});
+if(disable_modal){
+  
+disable_modal.addEventListener("click", function () {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+});
+}
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+toggleButton.addEventListener('click', function(){
+  //  mobileNav.style.display = 'block';
+    backdrop.style.display = "block";
+    mobileNav.style.transform = "translateX(0)";
+});
