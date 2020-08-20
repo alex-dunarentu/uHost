f5f2a450b5352e1aae8d082572e51f0c09e7ac18
Alexandru-Nicolae DUNĂRENŢU (102451) <alexandru.dunarentu@stud.etti.upb.ro>
2020-08-20 20:49:41 +0200

    formated code

--- shared.js
+++ shared.js
@@ -13,22 +13,22 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 backdrop.addEventListener("click", function () {
   backdrop.style.display = "none";
   mobileNav.style.transform = "translateX(-100%)";
-  if(modal){
-  modal.style.display = "none";}
- // mobileNav.style.display = 'none';
+  if (modal) {
+    modal.style.display = "none";
+  }
+  // mobileNav.style.display = 'none';
 });
-if(disable_modal){
-  
-disable_modal.addEventListener("click", function () {
-  backdrop.style.display = "none";
-  modal.style.display = "none";
-});
+if (disable_modal) {
+  disable_modal.addEventListener("click", function () {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+  });
 }
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav');
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
 
-toggleButton.addEventListener('click', function(){
+toggleButton.addEventListener("click", function () {
   //  mobileNav.style.display = 'block';
-    backdrop.style.display = "block";
-    mobileNav.style.transform = "translateX(0)";
+  backdrop.style.display = "block";
+  mobileNav.style.transform = "translateX(0)";
 });
